f734dc3521a75f829bc7fe9655fa59bf57206f49
Ivan <j2test.git@gmail.com>
2018-08-14 14:52:31 +0300

    FuzzBuzz commit with no continues

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,13 +6,13 @@ public class Main {
         for (int i = 0; i < 101; i++) {
             if(i%3==0&&i%5==0){
                 System.out.println("FizzBuzz");
-                continue;
+              //  continue;
             }else if(i%3==0&&i%5!=0){
                 System.out.println("Fizz");
-                continue;
+               // continue;
             }else if(i%3!=0&&i%5==0){
                 System.out.println("Buzz");
-                continue;
+             //   continue;
             }else{
                 System.out.println(i);
             }
